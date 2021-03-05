b8c3c93100efbd9109dcae89b7c9d37da934c70c
Nick Forrer <nwforrer@gmail.com>
2021-03-05 14:02:47 -0500

    Update encrypt-file-action to allow the public key path as a parameter.

--- file-encryption-platform-jar/src/main/java/io/github/nwforrer/actions/EncryptFileAction.java
+++ file-encryption-platform-jar/src/main/java/io/github/nwforrer/actions/EncryptFileAction.java
@@ -4,13 +4,16 @@ import io.github.nwforrer.encryption.GPGEncryptionUtil;
 import io.github.nwforrer.model.EncryptionModel;
 import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.model.ContentModel;
+import org.alfresco.repo.action.ParameterDefinitionImpl;
 import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ParameterDefinition;
+import org.alfresco.service.cmr.dictionary.DataTypeDefinition;
 import org.alfresco.service.cmr.repository.ContentReader;
 import org.alfresco.service.cmr.repository.ContentWriter;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.apache.commons.lang.StringUtils;
 import org.bouncycastle.openpgp.PGPException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +23,8 @@ import java.util.List;
 
 public class EncryptFileAction extends ActionExecuterAbstractBase {
     private static final Logger LOGGER = LoggerFactory.getLogger(EncryptFileAction.class);
+
+    public static final String PARAM_PUBLIC_KEY_PATH = "public_key_path";
 
     private final ServiceRegistry serviceRegistry;
     private final GPGEncryptionUtil gpgEncryptionUtil;
@@ -35,10 +40,15 @@ public class EncryptFileAction extends ActionExecuterAbstractBase {
     protected void executeImpl(Action action, NodeRef nodeRef) {
         LOGGER.info("Executing encrypt file action.");
 
+        String publicKeyPathParam = (String)action.getParameterValue(PARAM_PUBLIC_KEY_PATH);
+        String encryptionPublicKey = StringUtils.isEmpty(publicKeyPathParam) ? publicKeyPath : publicKeyPathParam;
+
+        LOGGER.info("Using public key at path: " + encryptionPublicKey);
+
         ContentReader reader = serviceRegistry.getContentService().getReader(nodeRef, ContentModel.PROP_CONTENT);
         ContentWriter writer = serviceRegistry.getContentService().getWriter(nodeRef, ContentModel.PROP_CONTENT, true);
 
-        try (InputStream publicKey = new FileInputStream(publicKeyPath);
+        try (InputStream publicKey = new FileInputStream(encryptionPublicKey);
              InputStream nodeContent = reader.getContentInputStream();
              OutputStream out = writer.getContentOutputStream()) {
             gpgEncryptionUtil.encryptFile(nodeContent, out, publicKey);
@@ -56,8 +66,8 @@ public class EncryptFileAction extends ActionExecuterAbstractBase {
     }
 
     @Override
-    protected void addParameterDefinitions(List<ParameterDefinition> list) {
-        // not needed
+    protected void addParameterDefinitions(List<ParameterDefinition> paramList) {
+        paramList.add(new ParameterDefinitionImpl(PARAM_PUBLIC_KEY_PATH, DataTypeDefinition.TEXT, false, "Public Key Path"));
     }
 
     public void setPublicKeyPath(String publicKeyPath) {
